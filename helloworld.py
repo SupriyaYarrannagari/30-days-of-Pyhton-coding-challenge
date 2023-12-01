edec297a7d81a03a50fbf2e14292d40ed750a6a7
SupriyaYarrannagari <142140859+SupriyaYarrannagari@users.noreply.github.com>
2023-12-01 21:03:56 +0530

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+# 30-days-of-Pyhton-coding-challenge
+# Introduction
+print(2+3)  #addition(+)
+print(5-4)  #subtraction(-)
+print(5*3)  #Multiplication(*)
+print(6/5)  #division(/)
+print(7%3)  #modulus(%)
+print(3**2)  #exponential(**)
+print(5//3)  #floor division operator(//)
+
+#checking Data Types
+print(type(10))                      #int
+print(type(3.45))                    #float
+print(type(4+5j))                    #complex
+print(type('sharvin tej'))           #string
+print(type([1,2,3,4,5]))             #list
+print(type((9,8,5,6,7,4)))           #Tuple
+print(type({"sharvin":" tej"}))      #Dictionary
+print(type({1.0,2.5,3.0,4.3,5}))     #set
